c8c85c26b28a49611df77b89e558d70a62a4505d
Deadpooldomlno <apopathodia@gmail.com>
2024-12-02 11:15:37 +0100

    Update BG_ninja.py

--- ninja pix/BG_ninja.py
+++ ninja pix/BG_ninja.py
@@ -1,93 +1,48 @@
-import pygame
-pygame.init()
+from game_ninja import Game #importation de la class Game
 
-class Background:
+import pygame #importatio de pygame
+pygame.init() #initialisation de pyagme
+
+     
+w,h = 680,550
+screen = pygame.display.set_mode((w,h)) #on defini la taille de la fenetre
+pygame.display.set_caption('ninja pix') #on choisi un nom
+
+#sol pas definitif
+sol = pygame.Surface((w,70))
+sol.fill((86, 101, 115))
+
+
+game = Game(screen) #instanciation de Game avec screen en argument
+
+FPS = pygame.time.Clock() 
+running = True
+
+#boucle principale
+while running:
     
-    def __init__(self,game):
-        self.game = game #permet de recuperer tous ce qui a dans la class de Game, car quand on fait l instance de Background dans la class game on met self(game) en argument
-        
-        #recupere les dimensions de l ecran
-        self.w = self.game.screen.get_width()  
-        self.h = self.game.screen.get_height()
-        
-        #atributs avec chaque image de layer en double et redimentioné et choisi une position et une vitesse pour chacune de ces images
-        self.layer1 = pygame.image.load('assets/bg/day/1.png') #calque 1
-        self.layer1 = pygame.transform.scale_by(self.layer1, 2) #redimensionnement
-        self.overlap1 = pygame.image.load('assets/bg/day/1.png') #double du calque 1
-        self.overlap1 = pygame.transform.scale_by(self.overlap1, 2) #redimensionnement
-        
-        self.l1Pos = 0 #position x de calque 1
-        self.o1Pos = -self.w #position x du double de calque 1
-        self.speed1 = 0.5 #vitesse
-        
-        
-        self.layer2 = pygame.image.load('assets/bg/day/2.png') #etc...
-        self.layer2 = pygame.transform.scale_by(self.layer2, 2)
-        self.overlap2 = pygame.image.load('assets/bg/day/2.png')
-        self.overlap2 = pygame.transform.scale_by(self.overlap2, 2)
-        
-        self.l2Pos = 0
-        self.o2Pos = -self.w
-        self.speed2 = 1.0
-        
-        
-        self.layer3 = pygame.image.load('assets/bg/day/3.png')
-        self.layer3 = pygame.transform.scale_by(self.layer3, 2)
-        self.overlap3 = pygame.image.load('assets/bg/day/3.png')
-        self.overlap3 = pygame.transform.scale_by(self.overlap3, 2)
-        
-        self.l3Pos = 0
-        self.o3Pos = -self.w
-        self.speed3 = 2.0
-        
-        
-        self.layer4 = pygame.image.load('assets/bg/day/4.png')
-        self.layer4 = pygame.transform.scale_by(self.layer4, 2)
-        self.overlap4 = pygame.image.load('assets/bg/day/4.png')
-        self.overlap4 = pygame.transform.scale_by(self.overlap4, 2)
-        
-        self.l4Pos = 0
-        self.o4Pos = -self.w
-        self.speed4 = 4.0
-        
-        #creation de differents tableau avec chacune des valeurs
-        self.layers = [self.layer1, self.layer2, self.layer3, self.layer4] #calques 
-        self.overlaps = [self.overlap1, self.overlap2, self.overlap3, self.overlap4] #double des calques
-        self.LPos = [self.l1Pos, self.l2Pos, self.l3Pos, self.l4Pos] #position x des calques 
-        self.OPos = [self.o1Pos ,self.o2Pos, self.o3Pos, self.o4Pos] #position x du double des calques 
-        self.speeds =[self.speed1, self.speed2, self.speed3, self.speed4] #vitesse
+    #affiche le sol et le background
+    game.bg.show_bg()
+    screen.blit(sol, (0,h-70))
     
-    #methode pour afficher les differents calques et double sur l ecran    
-    def show_bg(self): 
-        for i in range(4):
-            self.game.screen.blit(self.layers[i], (self.LPos[i],0))
-            self.game.screen.blit(self.overlaps[i], (self.OPos[i],0))
     
-    #methode pour bouger le background et faire l effet parralax      
-    def move_bg(self):
-        for i in range(4):
+    for shoot in game.player.all_shoots:
+        shoot.move()
+    
+    
+    screen.blit(game.player.current_image, game.player.rect) #affiche le joueur sur l ecran
+    game.player.move() #methode pour fair avancer le joeur
+    game.player.all_shoots.draw(screen) #dessine chaque shouriken contenue dans le groupe de sprite
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #condition pour fermer la fenetre
+            running = False
+        if event.type == pygame.KEYDOWN: #si une touche et pressée
+            if event.key == pygame.K_UP: #saute si fleche du haut est pressée
+                game.player.jump()
+            if event.key == pygame.K_SPACE: #lance un shuriken si barre d espace est pressee
+                game.player.shoot()
 
-            if self.game.player.last_direction == 'droite': #si le joueur va a droite
-                self.LPos[i] -= self.speeds[i]
-                self.OPos[i] -= self.speeds[i]
-
-                if self.LPos[i] + self.w <= 0:
-                    self.LPos[i] = self.OPos[i] + self.w
-                if self.OPos[i] + self.w <= 0: 
-                    self.OPos[i] = self.LPos[i] + self.w
-
-            elif self.game.player.last_direction == 'gauche': #si le joueur va a gauche
-                self.LPos[i] += self.speeds[i]
-                self.OPos[i] += self.speeds[i]
-                
-                if self.LPos[i] >= self.w: 
-                    self.LPos[i] = self.OPos[i] - self.w
-                if self.OPos[i] >= self.w:
-                    self.OPos[i] = self.LPos[i] - self.w
-
-                
-            
-            
-        
-        
-        
+    pygame.display.flip() #met ajoeur la fenetre
+    FPS.tick(60) #60 images par seconde
+pygame.quit() #fermeture de pygame
