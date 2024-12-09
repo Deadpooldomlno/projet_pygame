d6e43cecb4986d1857aa6b245ac65c2c88d2b0a1
Tirex45 <maia.theo0201@gmail.com>
2024-12-09 11:50:54 +0100

    Update game_ninja.py

--- ninja pix/game_ninja.py
+++ ninja pix/game_ninja.py
@@ -1,6 +1,6 @@
-from player_ninja import Player #imortation de la class Player
-from BG_ninja import Background #imortation de la class Background
-
+from player_ninja import Player #importation de la class Player
+from BG_ninja import Background #importation de la class Background
+from Ennemi import Ennemi
 
 import pygame
 pygame.init()
@@ -17,6 +17,16 @@ class Game():
         self.bg = Background(self) #instance de Background avec self(donc game) en atribut
         self.player = Player(self) #instance de Player avec self(donc game) en atribut
         
+        self.all_ennemis = pygame.sprite.Group() # Groupe de monstre
+        self.spawn_Ennemi()
         
- 
-        
+        self.all_players =pygame.sprite.Group()
+        self.player = Player(self)
+        self.all_players.add(self.player)
+    
+    def collision(self,sprite , group):
+        return pygame.sprite.spritecollide(sprite,group, False, pygame.sprite.collide_mask)
+    
+    def spawn_Ennemi(self):
+        ennemi= Ennemi(self)
+        self.all_ennemis.add(monster)
