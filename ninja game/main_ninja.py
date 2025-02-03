64509f98377139098347b21ccc1e6c4daef9e547
Tirex45 <maia.theo0201@gmail.com>
2025-02-03 11:43:53 +0100

    Update main_ninja.py

--- ninja game/main_ninja.py
+++ ninja game/main_ninja.py
@@ -6,7 +6,7 @@ pygame.init() #initialisation de pyagme
      
 w,h = 1152,718
 screen = pygame.display.set_mode((w,h)) #on defini la taille de la fenetre
-pygame.display.set_caption('ninja adventure') #on choisi un nom
+pygame.display.set_caption('ninja pix') #on choisi un nom
 
 #sol pas definitif
 sol = pygame.Surface((w,70))
@@ -28,13 +28,12 @@ while running:
     
     for shoot in game.player.all_shoots:
         shoot.move()
-        
     
-    
+    for ennemi in game.all_ennemis:
+        ennemi.draw(screen)
     screen.blit(game.player.current_image, game.player.rect) #affiche le joueur sur l ecran
     game.player.move() #methode pour fair avancer le joeur
     game.player.all_shoots.draw(screen) #dessine chaque shouriken contenue dans le groupe de sprite
-
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #condition pour fermer la fenetre
@@ -44,8 +43,9 @@ while running:
                 game.player.jump()
             if event.key == pygame.K_SPACE: #lance un shuriken si barre d espace est pressee
                 game.player.shoot()
-    
-    
-    pygame.display.flip() #met ajoeur la fenetre
+
+    pygame.display.flip() #met à jour la fenetre
     FPS.tick(60) #60 images par seconde
 pygame.quit() #fermeture de pygame
+    
+   
